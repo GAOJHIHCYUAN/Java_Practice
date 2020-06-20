8a2ee00b2f8ca719483050c5a6a39c2675f54e5a
AARON <kaochinchang@gmaiil.com>
2020-06-21 02:58:32 +0800

    Second Upload

--- _interview1.java
+++ _interview1.java
@@ -1,31 +1,42 @@
 import java.awt.*;
-import java.util.*;
+import java.awt.List;
+import java.util.ArrayList;
+import java.awt.List;
+import java.util.Scanner;
 
 public class _interview1 {
 	public static void main(String[]args) {
+		
+//		輸入一陣列，例如[1,2,3,4,5,6,7,8,9]
+//		取任意三個數字，加總起來等於8
+//		並輸出答案[1,2,5]、[1,3,4]
+		
 		ArrayList<Integer> arrlist = new ArrayList<Integer>();
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+//		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		int sum,number;
 		Scanner scanner = new Scanner(System.in);
 		number = scanner.nextInt();
 		System.out.println("Number is :" + number);
 		sum = scanner.nextInt();
 		System.out.println("Sum is :" + sum);
-		for (int i = 0 ; i <= number; i++) {
+		for (int i = 1 ; i <= number; i++) {
 			arrlist.add(i);
 		}
-		for (int i = 0 ; i<=arrlist.size()-2;i++) {
-			for(int j = i+1 ; j<=arrlist.size()-1 ; j++) {
-				for(int k=j+1 ; k<=arrlist.size(); k++) {
+		for (int i = 0 ; i<arrlist.size()-2;i++) {
+			for(int j = i+1 ; j<arrlist.size()-1 ; j++) {
+				for(int k=j+1 ; k<arrlist.size(); k++) {
 					if (arrlist.get(i) + arrlist.get(j) + arrlist.get(k) == sum) {
-						result.add(0,arrlist);
-						result.add(1,arrlist);
-						result.add(2,arrlist);
+						ArrayList<Integer> arr = new ArrayList<Integer>();
+						arr.add(arrlist.get(i));
+						arr.add(arrlist.get(j));
+						arr.add(arrlist.get(k));
+						result.add(arr);
 					}
 				}
 			}
 		}
-		System.out.print(arrlist);
-		System.out.print(result);
+		System.out.println(arrlist);
+		System.out.println(result);
 	}
 }
--- _interview2.java
+++ _interview2.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+//隨機創造一整數陣列，取出最大值、最小值，並計算平均。
+
 //public class _interview2 {
 //	public static void main(String[] args) {
 //		int [] arr = new int [10];
 //		for (int i=0 ; i < arr.length; i++) {
 //			int number = (int)(Math.random()*100);
 //			arr[i] = number;
-//			System.out.print(arr);
-//		}
 //
+//		}
+//		System.out.print(Arrays.toString(arr));
 //	}
 //}
 public class _interview2 {
@@ -19,8 +21,8 @@ public class _interview2 {
 		int[] x = new int[10];
 		int max,min;
 		float ave;
-		max = x[0];
-		min = x[0];
+		max = Integer.MAX_VALUE +1;
+		min = Integer.MIN_VALUE -1;
 		for (int i=0 ; i<x.length ; i++) {
 			x[i] = rand.nextInt(100)+1;
 			if(x[i] > max) {
@@ -33,7 +35,7 @@ public class _interview2 {
 		Arrays.sort(x);
 		System.out.println("Array is : \n" + Arrays.toString(x));
 		System.out.println("Max is:" + max + "\n" + "Min is:" + min);
-		ave = (float) ((max + min) / x.length);
+		ave = (float) ((max + min) / 2);
 		System.out.println("Average is :" + ave);
 	}
 }
